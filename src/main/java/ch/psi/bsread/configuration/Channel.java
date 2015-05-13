ad46f677e65c0bef5e384ffa4d3e0722d812f660
Fabian Märki <fabian.maerki@psi.ch>
2015-05-13 10:18:54 +0200

    Make Channel serializable.

--- src/main/java/ch/psi/bsread/configuration/Channel.java
+++ src/main/java/ch/psi/bsread/configuration/Channel.java
@@ -1,32 +1,40 @@
 package ch.psi.bsread.configuration;
 
-public class Channel {
+import java.io.Serializable;
+
+public class Channel implements Serializable {
+	private static final long serialVersionUID = 286422739407172968L;
 	
 	private String name;
-	private int offset = 0;
 	private double frequency = 100;
-	
-	public Channel(){
+	private int offset = 0;
+
+	public Channel() {
 	}
-	
-	public Channel(String name){
+
+	public Channel(String name) {
 		this.name = name;
 	}
-	
-	public Channel(String name, int offset, double frequency){
+
+	public Channel(String name, double frequency) {
 		this.name = name;
-		this.offset = offset;
 		this.frequency = frequency;
 	}
 	
+	public Channel(String name, double frequency, int offset) {
+		this.name = name;
+		this.frequency = frequency;
+		this.offset = offset;
+	}
+
 	public int getOffset() {
 		return offset;
 	}
-	
+
 	public void setOffset(int offset) {
 		this.offset = offset;
 	}
-	
+
 	public double getFrequency() {
 		return frequency;
 	}
@@ -34,14 +42,13 @@ public class Channel {
 	public void setFrequency(double frequency) {
 		this.frequency = frequency;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+
 }
