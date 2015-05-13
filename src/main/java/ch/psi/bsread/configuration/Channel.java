6baeb79259860e58352b8dd1aab191efcb9e6955
Fabian Märki <fabian.maerki@psi.ch>
2015-05-13 10:31:58 +0200

    Add hashCode, equals, and toString

--- src/main/java/ch/psi/bsread/configuration/Channel.java
+++ src/main/java/ch/psi/bsread/configuration/Channel.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class Channel implements Serializable {
 	private static final long serialVersionUID = 286422739407172968L;
-	
+
 	private String name;
 	private double frequency = 100;
 	private int offset = 0;
@@ -20,7 +20,7 @@ public class Channel implements Serializable {
 		this.name = name;
 		this.frequency = frequency;
 	}
-	
+
 	public Channel(String name, double frequency, int offset) {
 		this.name = name;
 		this.frequency = frequency;
@@ -51,4 +51,41 @@ public class Channel implements Serializable {
 		this.name = name;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(frequency);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + offset;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Channel other = (Channel) obj;
+		if (Double.doubleToLongBits(frequency) != Double.doubleToLongBits(other.frequency))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (offset != other.offset)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return name + " " + frequency + " " + offset;
+	}
 }
