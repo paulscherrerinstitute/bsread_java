246bfd9c5e162b30b76d74eded1c88d57d5d0a4f
Fabian Märki <fabian.maerki@psi.ch>
2016-10-27 10:50:09 +0200

    ATEST-571 and ATEST-563

--- src/main/java/ch/psi/bsread/monitors/ConnectionCounterMonitor.java
+++ src/main/java/ch/psi/bsread/monitors/ConnectionCounterMonitor.java
@@ -23,103 +23,114 @@ import ch.psi.bsread.message.commands.StopCommand;
 
 // builds on https://github.com/zeromq/jeromq/blob/master/src/test/java/zmq/TestMonitor.java
 public class ConnectionCounterMonitor implements Monitor {
-	private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionCounterMonitor.class);
-	private ExecutorService executor = Executors.newSingleThreadExecutor();
-	private AtomicInteger connectionCounter = new AtomicInteger();
-	private List<IntConsumer> handlers = new ArrayList<>();
-	private MonitorConfig monitorConfig;
+   private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionCounterMonitor.class);
+   private ExecutorService executor = Executors.newSingleThreadExecutor();
+   private AtomicInteger connectionCounter = new AtomicInteger();
+   private List<IntConsumer> handlers = new ArrayList<>();
+   private MonitorConfig monitorConfig;
 
-	public ConnectionCounterMonitor() {
-	}
+   public ConnectionCounterMonitor() {}
 
-	@Override
-	public void start(MonitorConfig monitorConfig) {
-		this.monitorConfig = monitorConfig;
+   @Override
+   public void start(MonitorConfig monitorConfig) {
+      this.monitorConfig = monitorConfig;
 
-		executor.execute(() -> {
-			String address = "inproc://" + monitorConfig.getMonitorItentifier();
-			Socket monitorSock = null;
-			try {
-				monitorConfig.getSocket().monitor(address, ZMQ.ZMQ_EVENT_ACCEPTED | ZMQ.ZMQ_EVENT_DISCONNECTED);
-				monitorSock = monitorConfig.getContext().socket(ZMQ.ZMQ_PAIR);
-				monitorSock.connect(address);
+      executor.execute(() -> {
+         String address = "inproc://" + monitorConfig.getMonitorItentifier();
+         Socket monitorSock = null;
+         try {
+            monitorConfig.getSocket().monitor(address, ZMQ.ZMQ_EVENT_ACCEPTED | ZMQ.ZMQ_EVENT_DISCONNECTED);
+            monitorSock = monitorConfig.getContext().socket(ZMQ.ZMQ_PAIR);
+            monitorSock.connect(address);
 
-				Event event;
-				// TestMonitor uses (event == null && s.errno() == ZError.ETERM)
-				// -> ?
-				while ((event = Event.read(monitorSock.base())) != null && monitorConfig.getSocket().errno() != ZError.ETERM
-						&& !Thread.currentThread().isInterrupted()) {
+            Event event;
+            // TestMonitor uses (event == null && s.errno() == ZError.ETERM)
+            // -> ?
+            while ((event = Event.read(monitorSock.base())) != null
+                  && monitorConfig.getSocket().errno() != ZError.ETERM
+                  && !Thread.currentThread().isInterrupted()) {
 
-					switch (event.event) {
-					case zmq.ZMQ.ZMQ_EVENT_ACCEPTED:
-						updateHandlers(connectionCounter.incrementAndGet());
-						break;
-					case zmq.ZMQ.ZMQ_EVENT_DISCONNECTED:
-						updateHandlers(connectionCounter.decrementAndGet());
-						break;
-					default:
-						LOGGER.info("Unexpected event '{}' received for identifier '{} monitoring '{}'", event.event,
-								monitorConfig.getMonitorItentifier(), event.addr);
-					}
-				}
-			} catch (Throwable e) {
-				LOGGER.warn("Monitoring zmq connections failed for identifier '{}'.", monitorConfig.getMonitorItentifier(), e);
-			} finally {
-				// Clear interrupted state as this might cause problems with the
-				// rest of the remaining code - i.e. closing of the zmq socket
-				// Thread.interrupted();
+               switch (event.event) {
+                  case zmq.ZMQ.ZMQ_EVENT_ACCEPTED:
+                     connectionCounter.incrementAndGet();
+                     updateHandlers();
+                     break;
+                  case zmq.ZMQ.ZMQ_EVENT_DISCONNECTED:
+                     connectionCounter.decrementAndGet();
+                     updateHandlers();
+                     break;
+                  default:
+                     LOGGER.info("Unexpected event '{}' received for identifier '{} monitoring '{}'", event.event,
+                           monitorConfig.getMonitorItentifier(), event.addr);
+               }
+            }
+         } catch (Throwable e) {
+            LOGGER.warn("Monitoring zmq connections failed for identifier '{}'.", monitorConfig.getMonitorItentifier(),
+                  e);
+         } finally {
+            // Clear interrupted state as this might cause problems with the
+            // rest of the remaining code - i.e. closing of the zmq socket
+            // Thread.interrupted();
 
-				if (monitorSock != null) {
-					monitorSock.close();
-				}
+            if (monitorSock != null) {
+               monitorSock.close();
+            }
 
-				connectionCounter.set(0);
-				updateHandlers(connectionCounter.get());
-				// clear references to ensure they can be gc
-				handlers.clear();
+            connectionCounter.set(0);
+            updateHandlers();
+            // clear references to ensure they can be gc
+            handlers.clear();
 
-				executor.shutdown();
-			}
-		});
-	}
+            executor.shutdown();
+         }
+      });
+   }
 
-	@Override
-	public void stop() {
-		try {
-			String stopCommandStr = monitorConfig.getObjectMapper().writeValueAsString(new StopCommand());
-			byte[] stopCommandByte = stopCommandStr.getBytes(StandardCharsets.UTF_8);
-			Msg msg = new Msg(stopCommandByte);
+   @Override
+   public synchronized void stop() {
+      handlers.clear();
+      
+      if (monitorConfig != null) {
+         try {
+            String stopCommandStr = monitorConfig.getObjectMapper().writeValueAsString(new StopCommand());
+            byte[] stopCommandByte = stopCommandStr.getBytes(StandardCharsets.UTF_8);
+            Msg msg = new Msg(stopCommandByte);
 
-			int nrOfStopMsgs = 1;
-			if (monitorConfig.getSocketType() == ZMQ.ZMQ_PUSH) {
-				nrOfStopMsgs = connectionCounter.get();
-			}
+            int nrOfStopMsgs = 1;
+            if (monitorConfig.getSocketType() == ZMQ.ZMQ_PUSH) {
+               nrOfStopMsgs = connectionCounter.get();
+            }
 
-			for (int i = 0; i < nrOfStopMsgs; ++i) {
-				// Receivers can react on it or not (see
-				// ReceiverConfig.keepListeningOnStop)
-				monitorConfig.getSocket().send(msg, ZMQ.ZMQ_NOBLOCK);
-			}
-		} catch (JsonProcessingException e) {
-			LOGGER.warn("Could not send stop command.", e);
-		}
+            for (int i = 0; i < nrOfStopMsgs; ++i) {
+               // Receivers can react on it or not (see
+               // ReceiverConfig.keepListeningOnStop)
+               monitorConfig.getSocket().send(msg, ZMQ.ZMQ_NOBLOCK);
+            }
+         } catch (JsonProcessingException e) {
+            LOGGER.warn("Could not send stop command.", e);
+         }
+      }
 
-		executor.shutdown();
-	}
+      executor.shutdown();
+   }
 
-	public int getConnectionCount() {
-		return connectionCounter.get();
-	}
+   public int getConnectionCount() {
+      return connectionCounter.get();
+   }
 
-	public void addHandler(IntConsumer handler) {
-		handlers.add(handler);
-	}
+   public synchronized void addHandler(IntConsumer handler) {
+      handler.accept(connectionCounter.get());
+      handlers.add(handler);
+   }
 
-	public void removeHandler(IntConsumer handler) {
-		handlers.remove(handler);
-	}
+   public synchronized void removeHandler(IntConsumer handler) {
+      handlers.remove(handler);
+   }
 
-	private void updateHandlers(int currentCounter) {
-		handlers.forEach(handler -> handler.accept(currentCounter));
-	}
+   private synchronized void updateHandlers() {
+      int currentCounter = connectionCounter.get();
+      for (IntConsumer handler : handlers) {
+         handler.accept(currentCounter);
+      }
+   }
 }
