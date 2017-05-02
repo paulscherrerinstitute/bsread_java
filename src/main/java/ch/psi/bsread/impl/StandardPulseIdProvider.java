7d9a310f186b85105c7c21090b2a5d363d33451d
Fabian Märki <fabian.maerki@psi.ch>
2017-05-02 14:41:50 +0200

    Option to provide inc.

--- src/main/java/ch/psi/bsread/impl/StandardPulseIdProvider.java
+++ src/main/java/ch/psi/bsread/impl/StandardPulseIdProvider.java
@@ -4,17 +4,24 @@ import ch.psi.bsread.PulseIdProvider;
 
 public class StandardPulseIdProvider implements PulseIdProvider {
 
-   // We start at one as the first pulseId should be 0
-   private long pulseId = 0;
+   private long pulseId;
+   private final long inc;
 
-   public StandardPulseIdProvider() {}
+   public StandardPulseIdProvider() {
+      this(-1);
+   }
 
    public StandardPulseIdProvider(long startPulseId) {
+      this(startPulseId, 1);
+   }
+
+   public StandardPulseIdProvider(long startPulseId, long inc) {
       this.pulseId = startPulseId;
+      this.inc = inc;
    }
 
    @Override
    public long getNextPulseId() {
-      return pulseId++;
+      return (pulseId += inc);
    }
 }
