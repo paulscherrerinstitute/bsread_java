acdf746a6f0e66eaa33908d6e53875a0678eade6
Fabian Märki <fabian.maerki@psi.ch>
2018-04-12 15:55:34 +0200

    toString and isInitialized.

--- src/main/java/ch/psi/bsread/common/concurrent/singleton/Deferred.java
+++ src/main/java/ch/psi/bsread/common/concurrent/singleton/Deferred.java
@@ -4,31 +4,34 @@ import java.util.Objects;
 import java.util.function.Supplier;
 
 /**
- * Lazy initialization of a Singleton when the blueprint is known at creation
- * time.
+ * Lazy initialization of a Singleton when the blueprint is known at creation time.
  */
 public final class Deferred<T> implements Supplier<T> {
-	private volatile Supplier<T> supplier = null;
-	private T object = null;
+   private volatile Supplier<T> supplier = null;
+   private T object = null;
 
-	public Deferred(Supplier<T> supplier) {
-		this.supplier = Objects.requireNonNull(supplier);
-	}
+   public Deferred(Supplier<T> supplier) {
+      this.supplier = Objects.requireNonNull(supplier);
+   }
 
-	public Deferred(T object) {
-		this.object = object;
-	}
+   public Deferred(T object) {
+      this.object = object;
+   }
 
-	@Override
-	public T get() {
-		if (supplier != null) {
-			synchronized (this) {
-				if (supplier != null) {
-					object = supplier.get();
-					supplier = null;
-				}
-			}
-		}
-		return object;
-	}
+   @Override
+   public T get() {
+      if (supplier != null) {
+         synchronized (this) {
+            if (supplier != null) {
+               object = supplier.get();
+               supplier = null;
+            }
+         }
+      }
+      return object;
+   }
+
+   public boolean isInitialized() {
+      return supplier == null;
+   }
 }
